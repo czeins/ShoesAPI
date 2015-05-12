5c65ffd3bc5ea51dc57de128c1aea95e5351f663
jemattei <jemattei@mac.com>
2015-05-12 15:34:25 -0700

    more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,41 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-shoes = Shoe.create([{ name: 'Dunk1', brand:'Nike', cost:30, color:"white" },{ name: 'Dunk2', brand:'Nike', cost:40, color:"black" },{ name: 'Dunk3', brand:'Nike', cost:50, color:"red" },{ name: 'Dunk4', brand:'Nike', cost:60, color:"green" },{ name: 'Dunk5', brand:'Nike', cost:70, color:"pink" },{ name: 'Dunk6', brand:'Nike', cost:80, color:"Gray" },{ name: 'Dunk7', brand:'Nike', cost:90, color:"black-white" },{ name: 'Dunk8', brand:'Nike', cost:100, color:"red-yellow" },{ name: 'Dunk9', brand:'Nike', cost:120, color:"green-yellow" } ])
+shoes = Shoe.create([
+  { name: 'Dunk1', brand:'Nike', cost:30, color:"white" },
+  { name: 'Dunk2', brand:'Nike', cost:40, color:"black" },
+  { name: 'Dunk3', brand:'Nike', cost:50, color:"red" },
+  { name: 'Dunk4', brand:'Nike', cost:60, color:"green" },
+  { name: 'Dunk5', brand:'Nike', cost:70, color:"pink" },
+  { name: 'Dunk6', brand:'Nike', cost:80, color:"Gray" },
+  { name: 'Dunk7', brand:'Nike', cost:90, color:"black-white" },
+  { name: 'Dunk8', brand:'Nike', cost:100, color:"red-yellow" },
+  { name: 'Dunk9', brand:'Nike', cost:120, color:"green-yellow" },
+  { name: 'HomeRun1', brand:'Adidas', cost:30, color:"red" },
+  { name: 'HomeRun2', brand:'Adidas', cost:40, color:"red-white" },
+  { name: 'HomeRun3', brand:'Adidas', cost:50, color:"red-black" },
+  { name: 'HomeRun4', brand:'Adidas', cost:60, color:"black-blue" },
+  { name: 'HomeRun5', brand:'Adidas', cost:70, color:"blue" },
+  { name: 'HomeRun6', brand:'Adidas', cost:80, color:"gray-white" },
+  { name: 'HomeRun7', brand:'Adidas', cost:90, color:"black-red" },
+  { name: 'HomeRun8', brand:'Adidas', cost:100, color:"black-yellow" },
+  { name: 'HomeRun9', brand:'Adidas', cost:120, color:"black-green" },
+  { name: 'Goal1', brand:'K-Swiss', cost:30, color:"white" },
+  { name: 'Goal2', brand:'K-Swiss', cost:40, color:"black" },
+  { name: 'Goal3', brand:'K-Swiss', cost:50, color:"red" },
+  { name: 'Goal4', brand:'K-Swiss', cost:60, color:"green" },
+  { name: 'Goal5', brand:'K-Swiss', cost:70, color:"pink" },
+  { name: 'Goal6', brand:'K-Swiss', cost:80, color:"Gray" },
+  { name: 'Goal7', brand:'K-Swiss', cost:90, color:"black-white" },
+  { name: 'Goal8', brand:'K-Swiss', cost:100, color:"red-yellow" },
+  { name: 'Goal9', brand:'K-Swiss', cost:120, color:"green-yellow" },
+  { name: 'Touchdown1', brand:'Easton', cost:30, color:"white" },
+  { name: 'Touchdown2', brand:'Easton', cost:40, color:"black" },
+  { name: 'Touchdown3', brand:'Easton', cost:50, color:"red" },
+  { name: 'Touchdown4', brand:'Easton', cost:60, color:"green" },
+  { name: 'Touchdown5', brand:'Easton', cost:70, color:"pink" },
+  { name: 'Touchdown6', brand:'Easton', cost:80, color:"Gray" },
+  { name: 'Touchdown7', brand:'Easton', cost:90, color:"black-white" },
+  { name: 'Touchdown8', brand:'Easton', cost:100, color:"red-yellow" },
+  { name: 'Touchdown9', brand:'Easton', cost:120, color:"green-yellow" }
+  ])
